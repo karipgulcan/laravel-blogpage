b9840f638ebab0a9724040330e22c5741b142111
karipgulcan <karipgulcan@gmail.com>
2021-02-09 20:39:25 +0300

    hmepageBlade

--- blog-sitesi/resources/views/front/homepage.blade.php
+++ blog-sitesi/resources/views/front/homepage.blade.php
@@ -7,62 +7,22 @@
     @section('content')
 
     <div class="col-lg-8 col-md-9 mx-auto">
+        @foreach($articles as $article)
         <div class="post-preview">
             <a href="post.html">
                 <h2 class="post-title">
-                    Man must explore, and this is exploration at its greatest
+                    {{$article->title}}
                 </h2>
                 <h3 class="post-subtitle">
-                    Problems look mighty small from 150 miles up
+                    {{Str::limit($article->content),12}}
                 </h3>
             </a>
-            <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 24, 2019
+            <p class="post-meta">
+                <a href="#">{{$article->category_id}}</a>
+                {{($article->created_at)}}
             </p>
         </div>
-        <hr>
-        <div class="post-preview">
-            <a href="post.html">
-                <h2 class="post-title">
-                    I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                </h2>
-            </a>
-            <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 18, 2019
-            </p>
-        </div>
-        <hr>
-        <div class="post-preview">
-            <a href="post.html">
-                <h2 class="post-title">
-                    Science has not yet mastered prophecy
-                </h2>
-                <h3 class="post-subtitle">
-                    We predict too much for the next year and yet far too little for the next ten.
-                </h3>
-            </a>
-            <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on August 24, 2019
-            </p>
-        </div>
-        <hr>
-        <div class="post-preview">
-            <a href="post.html">
-                <h2 class="post-title">
-                    Failure is not an option
-                </h2>
-                <h3 class="post-subtitle">
-                    Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                </h3>
-            </a>
-            <p class="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on July 8, 2019
-            </p>
-        </div>
+        @endforeach
         <hr>
         <!-- Pager -->
         <div class="clearfix">
