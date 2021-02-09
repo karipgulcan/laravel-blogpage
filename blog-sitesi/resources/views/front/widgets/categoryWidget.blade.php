717fcb4e8c4349903af78f2e188929911a7a5a58
karipgulcan <karipgulcan@gmail.com>
2021-02-09 17:25:51 +0300

    cadegory-widgets

---
+++ blog-sitesi/resources/views/front/widgets/categoryWidget.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">
+            Kategoriler
+        </div>
+        <div class="list-group">
+            @foreach($categories as $category)
+            <li class="list-group-item">
+                <a href="#">{{$category->name}}</a> <span class="badge bg-success float-right">12</span>
+            </li>
+            @endforeach
+        </div>
+    </div>
+</div>
