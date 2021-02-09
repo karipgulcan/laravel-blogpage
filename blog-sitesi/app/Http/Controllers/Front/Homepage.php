099cce94179470c88106143e87355497bb4588b6
karipgulcan <karipgulcan@gmail.com>
2021-02-09 20:38:53 +0300

    homepage-controller u5

--- blog-sitesi/app/Http/Controllers/Front/Homepage.php
+++ blog-sitesi/app/Http/Controllers/Front/Homepage.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Models
 use App\Models\Category;
+use App\Models\Article;
 
 class Homepage extends Controller
 {
     public function index(){
+        $data['articles']=Article::orderBy('created_at','DESC')->get();
         $data['categories']=Category::inRandomOrder()->get();
         return view('front.homepage',$data);
     }
