eca6c88a84d7874a0f1bf96ff630fcbfad8e8923
karipgulcan <karipgulcan@gmail.com>
2021-02-09 19:10:23 +0300

    articleMigration

---
+++ blog-sitesi/database/migrations/2021_02_09_142903_articles.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Articles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('category_id')->unsigned(); // category alanı categories tablomuzla ilişkili olduğu için unsigned sayesinde bağlantı kuracağız.
+            $table->string('title');
+            $table->string('image');
+            $table->longText('content');
+            $table->integer('hit')->default(0);
+            $table->string('slug');
+            $table->timestamps();
+
+
+            $table->foreign('category_id')->references('id')->on('categories'); // categories tablomuzdaki id ile bu tablodaki category_id mizi ilişkilendiriyoruz.
+            // onDelete('cascade') ->  Eğer categories tablosundan sütun silinirse bu tablodanda sil demekmiş.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
