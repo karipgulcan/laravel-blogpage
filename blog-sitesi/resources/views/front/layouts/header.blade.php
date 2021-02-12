c71500fa50fc37d692a130524ffa3cd40920933b
karipgulcan <karipgulcan@gmail.com>
2021-02-13 00:34:58 +0300

    header u3

--- blog-sitesi/resources/views/front/layouts/header.blade.php
+++ blog-sitesi/resources/views/front/layouts/header.blade.php
@@ -65,7 +65,7 @@
                 <div class="col-lg-8 col-md-10 mx-auto">
                     <div class="site-heading">
                         <h1>Clean Blog</h1>
-                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                        <span class="subheading">Hooop İsmail Abi...</span>
                     </div>
                 </div>
             </div>
