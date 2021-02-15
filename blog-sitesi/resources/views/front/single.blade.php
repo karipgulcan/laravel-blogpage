6263835e533d188ae02451f648e095bb0851c70d
karipgulcan <karipgulcan@gmail.com>
2021-02-15 21:19:11 +0300

    single-blade u6

--- blog-sitesi/resources/views/front/single.blade.php
+++ blog-sitesi/resources/views/front/single.blade.php
@@ -2,12 +2,15 @@
 
     @extends('front.layouts.master')
     @section('title')
+    @section('bg',$article->image)
     Kişisel Blog - Anasayfa
     @endsection
     @section('content') 
     <!-- Post Content -->
         <div class="col-lg-8 col-md-9 mx-auto">
             {!!$article->content!!}
+            <br><br><br><br><br><br>
+            <span class="text-danger">Okunma Sayısı: <b>{{$article->hit}}</b></span>
         </div>
     @include('front.widgets.categoryWidget')
     @endsection
