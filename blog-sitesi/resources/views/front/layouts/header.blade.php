c392ec235a9e1de4c1974d3bc0685cbcb5e5f362
karipgulcan <karipgulcan@gmail.com>
2021-02-15 22:40:32 +0300

    header-blade u5

--- blog-sitesi/resources/views/front/layouts/header.blade.php
+++ blog-sitesi/resources/views/front/layouts/header.blade.php
@@ -58,13 +58,13 @@
     </nav>
 
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('{{asset('front/')}}/img/home-bg.jpg')">
+    <header class="masthead" style="background-image: url('@yield('bg',asset('front/img/home-bg.jpg'))')">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
                     <div class="site-heading">
-                        <h1></h1>
+                        <h3>@yield('title')</h3>
                         <span class="subheading">Hooop İsmail Abi...</span>
                     </div>
                 </div>
