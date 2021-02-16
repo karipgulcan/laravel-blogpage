60a93462f35803afe7182bc0aa5ab0b4a717f399
karipgulcan <karipgulcan@gmail.com>
2021-02-17 00:14:15 +0300

    homepage

--- blog-sitesi/app/Http/Controllers/Front/Homepage.php
+++ blog-sitesi/app/Http/Controllers/Front/Homepage.php
@@ -12,7 +12,8 @@ use App\Models\Category;
 class Homepage extends Controller
 {
     public function index(){
-        $data['articles']=Article::orderBy('created_at','DESC')->get();
+        $data['articles']=Article::orderBy('created_at','DESC')->paginate(2);
+        $data['articles']->withPath(url('yazilar/sayfa'));
         $data['categories']=Category::inRandomOrder()->get();
         return view('front.homepage',$data);
     }
