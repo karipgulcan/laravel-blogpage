baab07092e05ed52dd7e0fe3ab92303fd0d73758
karipgulcan <karipgulcan@gmail.com>
2021-02-16 23:41:52 +0300

    header-blade

--- blog-sitesi/resources/views/front/layouts/header.blade.php
+++ blog-sitesi/resources/views/front/layouts/header.blade.php
@@ -31,7 +31,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand" href="{{route('homepage')}}">Hoşgeldiniz...</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                 data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -41,7 +41,7 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="{{route('homepage')}}">Anasayfa</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="about.html">About</a>
