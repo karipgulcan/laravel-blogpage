e1441d32703d81330130253fa407fa642eb94be5
karipgulcan <karipgulcan@gmail.com>
2021-02-17 22:18:25 +0300

    homepage-blade u16

--- blog-sitesi/resources/views/front/homepage.blade.php
+++ blog-sitesi/resources/views/front/homepage.blade.php
@@ -4,33 +4,7 @@
     @section('title','Anasayfa')
     @section('content')
     <div class="col-lg-8 col-md-9 mx-auto">
-        @foreach($articles as $article)
-        <div class="post-preview">
-            <a href="{{route('single',[$article->getCategory->slug,$article->slug])}}">
-                <h2 class="post-title">
-                    {{$article->title}}
-                </h2>
-                <img src="{{$article->image}}" alt="">
-                <h3 class="post-subtitle">
-                    {!!Str::limit($article->content),12!!}
-                </h3>
-            </a>
-            <p class="post-meta"> Kategori: <a href="#">{{$article->getCategory->name}}</a>
-                <span class="float-right">{{$article->created_at->diffForHumans()}}</span>
-            </p>
-        </div>
-        @if(!$loop->last) <!-- Eğer döngünün sonuysa çalışmasın. Yani son posttan sonra hr koymasın diye koyduk-->
-        <hr>
-        @endif
-        @endforeach
-
-        {{$articles->links()}} <!-- pagination için / hangi değişken içinde yaptıysak onu yazıyoruz. -->
-
-        <!-- Pager 
-        <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-        </div>
-        -->
+        @include('front.widgets.articleList')
     </div>
     @include('front.widgets.categoryWidget')
     @endsection
