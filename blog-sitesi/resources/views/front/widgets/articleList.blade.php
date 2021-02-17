f33ac5f6f173dd5c7e20a4758770050bd3fd158c
karipgulcan <karipgulcan@gmail.com>
2021-02-17 22:18:40 +0300

    articleList-blace created

---
+++ blog-sitesi/resources/views/front/widgets/articleList.blade.php
@@ -0,0 +1,32 @@
+@if (count($articles)>0)
+@foreach($articles as $article)
+<div class="post-preview">
+    <a href="{{route('single',[$article->getCategory->slug,$article->slug])}}">
+        <h2 class="post-title">
+            {{$article->title}}
+        </h2>
+        <img src="{{$article->image}}" alt="">
+        <h3 class="post-subtitle">
+            {!!Str::limit($article->content),12!!}
+        </h3>
+    </a>
+    <p class="post-meta"> Kategori: <a href="#">{{$article->getCategory->name}}</a>
+        <span class="float-right">{{$article->created_at->diffForHumans()}}</span>
+    </p>
+</div>
+@if(!$loop->last)
+<!-- Eğer döngünün sonuysa çalışmasın. Yani son posttan sonra hr koymasın diye koyduk-->
+<hr>
+@endif
+@endforeach
+
+<div class="float-center">
+    {{$articles->links()}}
+</div>
+
+
+@else
+<div class="alert alert-danger">
+    <h3>Bu kategoriye ait yazı bulunamadı.</h3>
+</div>
+@endif
