1f6cc3bbe8e31c4682ce3a7fed9e43bdca7252f9
karipgulcan <karipgulcan@gmail.com>
2021-02-17 23:04:59 +0300

    homepage

--- blog-sitesi/app/Http/Controllers/Front/Homepage.php
+++ blog-sitesi/app/Http/Controllers/Front/Homepage.php
@@ -32,7 +32,7 @@ class Homepage extends Controller
     public function category($slug){
         $category=Category::where('slug',$slug)->first() ?? abort(403,'Böyle bir kategori henüz eklenmedi.');
         $data['category']=$category;
-        $data['articles']=Article::where('category_id', $category->id)->orderBy('created_at','DESC')->get();
+        $data['articles']=Article::where('category_id', $category->id)->orderBy('created_at','DESC')->paginate(2);
         $data['categories']=Category::inRandomOrder()->get();
         return view('front.category',$data);
     }
