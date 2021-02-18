10bd00f0ec2766162d860b19008e4b6ef7fe78b5
karipgulcan <karipgulcan@gmail.com>
2021-02-18 22:19:27 +0300

    Homepage

--- blog-sitesi/app/Http/Controllers/Front/Homepage.php
+++ blog-sitesi/app/Http/Controllers/Front/Homepage.php
@@ -8,13 +8,19 @@ use Illuminate\Http\Request;
 // Models
 use App\Models\Article;
 use App\Models\Category;
+use App\Models\Page;
+
 
 class Homepage extends Controller
 {
+    public function __construct(){
+        view()->share('pages',$data['pages']=Page::orderBy('order','ASC')->get()); //bu data tüm viewlerde çalışıyor
+        view()->share('categories',$data['categories']=Category::inRandomOrder()->get()); 
+    }
+
     public function index(){
         $data['articles']=Article::orderBy('created_at','DESC')->paginate(2);
         $data['articles']->withPath(url('/sayfa'));
-        $data['categories']=Category::inRandomOrder()->get();
         return view('front.homepage',$data);
     }
 
@@ -22,18 +28,23 @@ class Homepage extends Controller
 
         $category=Category::where('slug',$category)->first() ?? abort(403,'Böyle bir kategori henüz eklenmedi.');
         $article=Article::where('slug',$slug)->first() ?? abort(403,'Böyle bir yazı henüz yazılmadı.');
-        //dd($article);
         $article->increment('hit');
         $data['article']=$article;
-        $data['categories']=Category::inRandomOrder()->get();
         return view('front.single',$data);
     }
 
     public function category($slug){
+
         $category=Category::where('slug',$slug)->first() ?? abort(403,'Böyle bir kategori henüz eklenmedi.');
         $data['category']=$category;
         $data['articles']=Article::where('category_id', $category->id)->orderBy('created_at','DESC')->paginate(2);
-        $data['categories']=Category::inRandomOrder()->get();
         return view('front.category',$data);
+    }
+
+    public function page($slug){
+
+        $page=Page::where('slug',$slug)->first() ?? abort(403,'Böyle bir sayfa bulunamadı.');
+        $data['page']=$page;
+        return view('front.page',$data);
     }
 }
