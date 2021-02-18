2d287222079d22faa13a2b8c755e26d0f19c757f
karipgulcan <karipgulcan@gmail.com>
2021-02-18 22:21:15 +0300

    page-blade created

---
+++ blog-sitesi/resources/views/front/page.blade.php
@@ -0,0 +1,10 @@
+@extends('front.layouts.master')
+@section('title')
+@section('bg',$page->image)
+@section('content') 
+    <div class="col-lg-8 col-md-9 mx-auto">
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+    </div>
+@endsection
