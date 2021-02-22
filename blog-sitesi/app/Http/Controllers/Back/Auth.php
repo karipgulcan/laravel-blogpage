19c1735419f858c8b0e3db29745c0c13a1d618b6
karipgulcan <karipgulcan@gmail.com>
2021-02-22 21:43:14 +0300

    Auth Controller created

---
+++ blog-sitesi/app/Http/Controllers/Back/Auth.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Back;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Auth extends Controller
+{
+    public function login(){
+        return view('back.auth.login');
+    }
+}
