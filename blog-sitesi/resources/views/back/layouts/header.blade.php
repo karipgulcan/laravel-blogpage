4cfdaecb4eadaae6320dcdc5ae665505b1ab6d25
karipgulcan <karipgulcan@gmail.com>
2021-02-22 21:43:59 +0300

    back header-blade u2

--- blog-sitesi/resources/views/back/layouts/header.blade.php
+++ blog-sitesi/resources/views/back/layouts/header.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>@yield('Blog Sitesi','Yönetim Paneli')</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('back/')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
