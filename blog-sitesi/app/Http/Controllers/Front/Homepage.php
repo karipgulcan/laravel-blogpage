17581a116c6334a6318eb127d180fc447644e75b
karipgulcan <karipgulcan@gmail.com>
2021-02-22 19:50:23 +0300

    +

--- blog-sitesi/app/Http/Controllers/Front/Homepage.php
+++ blog-sitesi/app/Http/Controllers/Front/Homepage.php
@@ -5,20 +5,26 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Validator;
+
 // Models
 use App\Models\Article;
 use App\Models\Category;
 use App\Models\Page;
+use App\Models\Contact;
+
 
 
 class Homepage extends Controller
 {
     public function __construct(){
+        
         view()->share('pages',$data['pages']=Page::orderBy('order','ASC')->get()); //bu data tüm viewlerde çalışıyor
         view()->share('categories',$data['categories']=Category::inRandomOrder()->get()); 
     }
 
     public function index(){
+        
         $data['articles']=Article::orderBy('created_at','DESC')->paginate(2);
         $data['articles']->withPath(url('/sayfa'));
         return view('front.homepage',$data);
@@ -46,5 +52,39 @@ class Homepage extends Controller
         $page=Page::where('slug',$slug)->first() ?? abort(403,'Böyle bir sayfa bulunamadı.');
         $data['page']=$page;
         return view('front.page',$data);
+    }
+
+    public function contact(){
+        return view('front.contact');
+    }
+
+    public function contactpost(Request $request){
+
+        $rules =[
+            'name'=>'required|min:2',
+            'email'=>'required|email',
+            'topic'=>'required',
+            'message'=>'required|min:7'
+        ];
+
+
+        $validate=Validator::make($request->post(),$rules); // olan değerlerin kontrollerini sağlıyor
+
+        if($validate->fails()){
+
+            return redirect()->route('contactpost')->withErrors($validate)->withInput();
+            //print_r($validate->errors()->first('message'));
+
+        }
+        die;
+
+        $contact = new Contact;
+        $contact->name=$request->name;
+        $contact->email=$request->email;
+        $contact->topic=$request->topic;
+        $contact->message=$request->message;
+        $contact->save();
+        return redirect()->route('contactpost')->with('success','Mesajınız bize iletildi.'); //iletişim sayfasının url si
+        //print_r($request->post());
     }
 }
