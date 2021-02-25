81f0f7b495d76841e066076ff63028602c0e0060
karipgulcan <karipgulcan@gmail.com>
2021-02-25 22:56:20 +0300

    admin model u2

--- blog-sitesi/app/Models/Admin.php
+++ blog-sitesi/app/Models/Admin.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable; //Authentication için 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
