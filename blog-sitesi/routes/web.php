8cff385992f1fdd38812e885198be40a76ac035a
karipgulcan <karipgulcan@gmail.com>
2021-02-25 20:50:19 +0300

    web u

--- blog-sitesi/routes/web.php
+++ blog-sitesi/routes/web.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('admin/panel','App\Http\Controllers\Back\Dashboard@index')->name('admin.dashboard');
-Route::get('admin/giris','App\Http\Controllers\Back\Auth@login')->name('admin.login');
-Route::post('admin/giris','App\Http\Controllers\Back\Auth@loginPost')->name('admin.login.post');
+Route::get('admin/giris','App\Http\Controllers\Back\AuthController@login')->name('admin.login');
+Route::post('admin/giris','App\Http\Controllers\Back\AuthController@loginPost')->name('admin.login.post');
 
 
 /*
