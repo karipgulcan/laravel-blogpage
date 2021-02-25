ed12f3e921d8ca4e991a78e2db4985f8796a7f5e
karipgulcan <karipgulcan@gmail.com>
2021-02-25 22:57:23 +0300

    login blade u

--- blog-sitesi/resources/views/back/auth/login.blade.php
+++ blog-sitesi/resources/views/back/auth/login.blade.php
@@ -41,6 +41,11 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Hoşgeldiniz</h1>
                                     </div>
+                                    @if($errors->any())
+                                    <div class="alert alert-danger">
+                                        {{$errors->first()}}
+                                    </div>
+                                    @endif
                                     <form method="POST" action="{{route('admin.login.post')}}" class="user">
                                         @csrf
                                         <div class="form-group">
@@ -51,12 +56,6 @@
                                         <div class="form-group">
                                             <input type="password" name="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Şifre">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Şifremi Hatırla</label>
-                                            </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Giriş
