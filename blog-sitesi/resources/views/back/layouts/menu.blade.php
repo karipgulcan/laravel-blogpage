78fbc14876a990389674c68ecbc0ae7e7bab0f32
karipgulcan <karipgulcan@gmail.com>
2021-03-14 20:46:01 +0300

    menu

--- blog-sitesi/resources/views/back/layouts/menu.blade.php
+++ blog-sitesi/resources/views/back/layouts/menu.blade.php
@@ -39,7 +39,7 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Makale İşlemleri:</h6>
-                        <a class="collapse-item" href="buttons.html">Tüm Makaleler</a>
+                        <a class="collapse-item" href="{{route('admin.makaleler.index')}}">Tüm Makaleler</a>
                         <a class="collapse-item" href="cards.html">Makale Oluştur</a>
                     </div>
                 </div>
