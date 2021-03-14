45a069453d5afd320c1570b6fde67f28289c8772
karipgulcan <karipgulcan@gmail.com>
2021-03-14 18:57:35 +0300

    menu

--- blog-sitesi/resources/views/back/layouts/menu.blade.php
+++ blog-sitesi/resources/views/back/layouts/menu.blade.php
@@ -16,9 +16,9 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{route('admin.dashboard')}}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Panel</span></a>
             </li>
 
             <!-- Divider -->
@@ -26,7 +26,7 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                İçerik Yönetimi
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
@@ -34,13 +34,13 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Makaleler</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <h6 class="collapse-header">Makale İşlemleri:</h6>
+                        <a class="collapse-item" href="buttons.html">Tüm Makaleler</a>
+                        <a class="collapse-item" href="cards.html">Makale Oluştur</a>
                     </div>
                 </div>
             </li>
