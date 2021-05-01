28fac6b0fd3da67c7a10e0f0147ef64291317908
karipgulcan <karipgulcan@gmail.com>
2021-05-01 14:59:24 +0300

    index blade created

--- blog-sitesi/resources/views/back/articles/index.blade.php
+++ blog-sitesi/resources/views/back/articles/index.blade.php
@@ -1,7 +1,47 @@
-@extend('back.layouts.master');
+@extends('back.layouts.master');
 @section('title', 'Tüm Makaleler');
 @section('content')
-
-    burası articles
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">@yield('title')</h6>
+        <h6 class="m-0 font-weight-bold float-right text-primary">{{$articles->count()}} Makale bulundu.</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Fotoğraf</th>
+                        <th>Makale Başlığı</th>
+                        <th>Kategori</th>
+                        <th>Hit</th>
+                        <th>Oluşturulma Tarihi</th>
+                        <th>Durum</th>
+                        <th>İşlemler</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($articles as $article)
+                    <tr>
+                        <td>
+                            <img src="{{$article->image}}" width="200">
+                        </td>
+                        <td>{{$article->title}}</td>
+                        <td>{{$article->getCategory->name}}</td>
+                        <td>{{$article->hit}}</td>
+                        <td>{{$article->created_at->diffForHumans()}}</td>
+                        <td>{!!$article->status==0 ? "<span class='text-danger'> Pasif </span>" : "<span class='text-success'> Aktif </span>" !!}</td>
+                        <td>
+                            <a href="" title="Görüntüle" class="btn btn-sm btn-success"><i class="fa fa-eye"></i></a>
+                            <a href="" title="Düzenle" class="btn btn-sm btn-primary"><i class="fa fa-pen"></i></a>
+                            <a href="" title="Sil" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 @endsection
