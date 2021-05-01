a525b5712cab4c5dcee759377255337a2c466583
karipgulcan <karipgulcan@gmail.com>
2021-05-01 15:00:13 +0300

    datebase seeder updated

--- blog-sitesi/database/seeders/DatabaseSeeder.php
+++ blog-sitesi/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(ArticleSeeder::class);
         $this->call(PageSeeder::class);
-        $this->call(AuthSeeder::class);
+        $this->call(AdminSeeder::class);
 
     }
 }
