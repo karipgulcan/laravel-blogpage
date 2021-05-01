2eda0463db670e2f471b279ddc34def5b09e8f41
karipgulcan <karipgulcan@gmail.com>
2021-05-01 14:59:55 +0300

    header blade updated

--- blog-sitesi/resources/views/back/layouts/header.blade.php
+++ blog-sitesi/resources/views/back/layouts/header.blade.php
@@ -19,5 +19,6 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('back/')}}/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{asset('back/')}}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
