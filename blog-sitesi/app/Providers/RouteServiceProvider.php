ae5bff18cb5755aad2d9a3edd1857104a4b01504
karipgulcan <karipgulcan@gmail.com>
2021-05-03 19:07:04 +0300

    RouteProviders

--- blog-sitesi/app/Providers/RouteServiceProvider.php
+++ blog-sitesi/app/Providers/RouteServiceProvider.php
@@ -49,7 +49,8 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         Route::resourceVerbs([
-            'create' => 'olustur'
+            'create' => 'olustur',
+            'edit' => 'guncelle'
           ]); 
     }
 
