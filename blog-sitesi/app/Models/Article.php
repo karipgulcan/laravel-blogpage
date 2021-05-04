42310f2c3949db7217acfd70edd284132487c9a7
karipgulcan <karipgulcan@gmail.com>
2021-05-04 11:55:02 +0300

    articleModel

--- blog-sitesi/app/Models/Article.php
+++ blog-sitesi/app/Models/Article.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Article extends Model
 {
-    //use HasFactory;
+    use SoftDeletes;
 
     function getCategory(){
        return $this->hasOne('App\Models\Category','id','category_id');
