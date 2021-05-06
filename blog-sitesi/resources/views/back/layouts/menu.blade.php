030f233669a30c37985a3c76cf3df84b2fe576ed
karipgulcan <karipgulcan@gmail.com>
2021-05-06 14:38:26 +0300

    menu blade

--- blog-sitesi/resources/views/back/layouts/menu.blade.php
+++ blog-sitesi/resources/views/back/layouts/menu.blade.php
@@ -54,6 +54,21 @@
                 </a>
             </li>
 
+            <li class="nav-item">
+                <a class="nav-link @if(Request::segment(2)=="sayfalar") in @else collapsed @endif" href="#" data-toggle="collapse" data-target="#collapsePage"
+                    aria-expanded="true" aria-controls="collapsePage">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Sayfalar</span>
+                </a>
+                <div id="collapsePage" class="collapse @if(Request::segment(2)=="sayfalar") show @endif" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Sayfa İşlemleri:</h6>
+                        <a class="collapse-item @if(Request::segment(2)=="sayfalar" and !Request::segment(3)) active @endif" href="{{route('admin.page.index')}}">Tüm Sayfalar</a>
+                        <a class="collapse-item @if(Request::segment(2)=="sayfalar" and Request::segment(3)=="olustur") active @endif" href="{{route('admin.page.create')}}">Sayfa Oluştur</a>
+                    </div>
+                </div>
+            </li>
+
             <!-- Divider -->
             <hr class="sidebar-divider">
 
