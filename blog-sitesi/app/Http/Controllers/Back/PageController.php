cf9151b1cd2827438faa1047b3ddc5bd6a93b29e
karipgulcan <karipgulcan@gmail.com>
2021-05-06 14:37:17 +0300

    PageController created

---
+++ blog-sitesi/app/Http/Controllers/Back/PageController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Back;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Page;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+
+class PageController extends Controller
+{
+    public function index(){
+        $pages=Page::all();
+        return view('back.pages.index',compact('pages'));
+    }
+    public function create(){
+        return view('back.pages.create');
+    } 
+    public function update($id){
+        $page=Page::findOrFail($id);
+        return view('back.pages.update',compact('page'));
+    }
+
+    public function updatePost(Request $request, $id)
+    {
+        //return "geldi";
+        $request->validate([
+            'title'=>'min:3',
+            'image'=>'image|mimes:jpeg,png,jpg|max:1500'
+        ]);
+        //dd($request->post());
+        $page=Page::findOrFail($id);
+        $page->title=$request->title;
+        $page->content=$request->content;
+        $page->slug=Str::slug($request->title);//url yi uyumlu hale getirip kaydedecek
+
+        if($request->hasFile('image')){
+            $imageName=Str::slug($request->title).'.'.$request->image->getClientOriginalExtension();
+            $request->image->move(public_path('uploads'),$imageName);
+            $page->image='http://localhost:8000/uploads/'.$imageName;
+        }
+
+        $page->save();
+        toastr()->success('Başarılı', 'Sayfa başarı ile güncellendi');
+        return redirect()->route('admin.page.index');
+    }
+
+    public function post(Request $request){
+        $request->validate([
+            'title'=>'min:3',
+            'image'=>'required|image|mimes:jpeg,png,jpg|max:1500'
+        ]);
+
+        $last=Page::orderBy('order','desc')->first();
+
+        //dd($request->post());
+        $page= new Page;
+        $page->title=$request->title;
+        $page->content=$request->content;
+        $page->order=$last->order+1;
+        $page->slug=Str::slug($request->title);//url yi uyumlu hale getirip kaydedecek
+
+        if($request->hasFile('image')){
+            $imageName=Str::slug($request->title).'.'.$request->image->getClientOriginalExtension();
+            $request->image->move(public_path('uploads'),$imageName);
+            $page->image='http://localhost:8000/uploads/'.$imageName;
+        }
+
+        $page->save();
+        toastr()->success('Sayfa başarı ile oluşturuldu');
+        return redirect()->route('admin.page.index');
+    }
+
+    public function switch(Request $request){
+        //return $request->id;
+        $page=Page::findOrFail($request->id);
+        $page->status=$request->statu=="true" ? 1 : 0 ;
+        $page->save();
+    }
+}
