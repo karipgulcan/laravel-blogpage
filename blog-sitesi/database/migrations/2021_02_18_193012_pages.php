de7f5ce5d639bfa610e6246915c4f8dd3ba37419
karipgulcan <karipgulcan@gmail.com>
2021-05-06 14:37:31 +0300

    pages migration created

--- blog-sitesi/database/migrations/2021_02_18_193012_pages.php
+++ blog-sitesi/database/migrations/2021_02_18_193012_pages.php
@@ -20,6 +20,7 @@ class Pages extends Migration
             $table->longText('content');
             $table->string('slug');
             $table->integer('order');
+            $table->integer('status')->default(1);
             $table->timestamps();
         });
     }
