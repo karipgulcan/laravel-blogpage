1573f191ef7834ebc59328e2d1b37aadbebfcf58
karipgulcan <karipgulcan@gmail.com>
2021-05-09 12:38:19 +0300

    Dashboard

--- blog-sitesi/app/Http/Controllers/Back/Dashboard.php
+++ blog-sitesi/app/Http/Controllers/Back/Dashboard.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Page;
 
 class Dashboard extends Controller
 {
     public function index(){
-        return view('back.dashboard');
+        $article=Article::all()->count();
+        $hit=Article::sum('hit');
+        $category=Category::all()->count();;
+        return view('back.dashboard',compact('article','hit','category'));
     }
 }
